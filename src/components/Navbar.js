15448c0f13c3773ae4ee8ba145b084e32c75005b
kanu-CH <kanika.251203@gmail.com>
2024-08-01 19:45:38 +0530

    updated files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import logo from '../assets/plantimages/logo.png';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/plantimages/logo.png';
 import './Navbar.css';
 
 function NavbarComponent() {
