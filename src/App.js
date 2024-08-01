01577f63e8b6586a73020c3bf3e8775e047afb40
Kanika Chaudhary <121243650+kanu-CH@users.noreply.github.com>
2024-08-01 18:58:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavbarComponent from './components/Navbar';
-import Hero from './components/Hero';
-import Service from './components/Service';
 import Footer from './components/Footer';
-import About from './components/About';
-import Plant from './components/Plant';
-import Features from './components/Features';
-import Process from './components/Process';
-import Booking from './components/Booking';
+import Home from './components/Home';  
+// import Events from './pages/Events';  
+// import Blog from './pages/Blog';      
 import './assets/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavbarComponent />
-      <Hero />
-      <About />
-      <Service />
-      <Plant />
-      <Features />
-      <Process />
-      <Booking />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/events" element={<Events />} />      */}
+        {/* <Route path="/blog" element={<Blog />} />          */}
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
