b3e8d3e3354992ada22fbde2b760c14e433a6fd4
udityaprakash <udityaprakash983@gmail.com>
2024-08-01 21:42:50 +0530

    resolved conflicts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Service from '../components/Service';
-import Features from '../components/Features';
-import Plant from '../components/Plant';
-import Process from '../components/Process';
-import Booking from '../components/Booking';
-
-function Home() {
-  return (
-    <div>
-      <Hero />
-      <About />
-      <Service />
-      <Features />
-      <Plant />
-      <Process />
-      <Booking />
-    </div>
-  );
-}
-
-export default Home;
-=======
 
 import React from 'react';
 import Hero from '../components/Hero';
@@ -49,4 +23,3 @@ function Home() {
 }
 
 export default Home;
->>>>>>> bc7d73381063a381c77b2ead29140c6f54abab67
